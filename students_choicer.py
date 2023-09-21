bee3f0fa092b1d3a7b1db5eb1f224a6a8bf6eeca
314 student <test@gmail.com>
2023-09-21 20:25:46 +0200

    choicer menu 0.1

--- students_choicer.py
+++ students_choicer.py
@@ -1,1 +1,22 @@
+import random
 
+
+def students_from_file():
+    pass
+
+
+def students_from_console():
+    pass
+
+
+def students_choicer():
+    print("Привет. Это рандомайзер выбора студента!")
+    print("Список студентов будет заполнен сейчас или взят из файла?")
+    print("1. Заполнен сейчас")
+    print("2. Взят из файла")
+    user_choice = input("Ваш выбор: ")
+
+
+
+if __name__ == "__main__":
+    pass
