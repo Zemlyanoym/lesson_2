58aa8443d11aa18f0356e3e5a9972f0153c12b2e
314 student <12@2.rr>
2023-09-21 20:30:12 +0200

    alpha version 0.5

--- rps.py
+++ rps.py
@@ -13,7 +13,20 @@ while True:
     print("1 - камень")
     print("2 - ножницы")
     print("3 - бумага")
-    user_hand = int(input("Введите номер: "))
+    user_hand = input("Введите номер: ")
+
+    if len(user_hand) != 1:
+        print("Необходимо вводить одну цифру!")
+        continue
+    if user_hand not in "123":
+        print("Допустимые значения: 1, 2 ,3")
+        continue
+    if user_hand.isdecimal():
+        user_hand = int(user_hand)
+    else:
+        print("Необходимо вводить только цифры!")
+        continue
+
     print(f"Bot: {bot_hand}, you: {hands[user_hand-1]}")
     if win_hands[bot_hand] == hands[user_hand-1]:
         print("Bot win", win_hands[bot_hand])
