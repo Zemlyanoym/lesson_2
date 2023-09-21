8aad3ab79e9a7e2c19de6a256060ebadc86ef08a
314 student <test@gmail.com>
2023-09-21 20:15:43 +0200

    all cases

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from word_searcher import word_searcher
 
 
 if __name__ == "__main__":
-
     print("Привет. Это самая полезная программа в мире.")
     print("Ты можешь выбрать любое из 6 приложений:")
     print("1. Калькулятор от Павла")
@@ -14,3 +13,11 @@ if __name__ == "__main__":
     print("3. 'Камень, ножницы, бумага' от Дмитрия")
     print("4. 'Крестики-нолики' от Михаила")
     print("5. 'Рандомайзер выбора студентов' от ...")
+    user_choice = int(input("Ваш выбор: "))
+    match user_choice:
+        case 1: calc_pavel()
+        case 2: calc_mihail()
+        case 3: rps()
+        case 4: students_choicer()
+        case 5: word_searcher()
+        case _: print("Неизвестная команда.")
