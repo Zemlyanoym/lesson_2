6d17b1ab62f2342b96f0f9c4ec1c959b7edaff9c
314 student <test@gmail.com>
2023-09-21 20:11:08 +0200

    all imports and temp menu

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from calc import calc_pavel
+from calcMihail import calc_mihail
+from rps import rps
+from students_choicer import students_choicer
+from word_searcher import word_searcher
 
+
+if __name__ == "__main__":
+    print("Привет. Это самая полезная программа в мире.")
+    print("Ты можешь выбрать любое из 6 приложений:")
+    print("1. Калькулятор от Павла")
+    print("2. Калькулятор от Михаила")
+    print("3. 'Камень, ножницы, бумага' от Дмитрия")
+    print("4. 'Крестики-нолики' от Михаила")
+    print("5. 'Рандомайзер выбора студентов' от ...")
