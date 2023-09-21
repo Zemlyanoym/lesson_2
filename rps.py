c785b46e20448b564043f1b28cda8987e4cce836
314 student <12@2.rr>
2023-09-21 20:18:30 +0200

    alpha version

--- rps.py
+++ rps.py
@@ -1,15 +1,28 @@
 import random
-
+hands = ["камень", "ножницы", "бумага"]
 win_hands = {"камень": "ножницы",
              "ножницы": "бумага",
-             "бумага": "камень"
+             "бумага": "камень",
+
              }
 
 
 while True:
-    bot_hand = random.choice(("камень", "ножницы", "бумага"))
+    bot_hand = random.choice(hands)
+
     print("1 - камень")
     print("2 - ножницы")
     print("3 - бумага")
-    user_hand = input("Введите номер: ")
+    user_hand = int(input("Введите номер: "))
+    print(f"Bot: {bot_hand}, you: {hands[user_hand-1]}")
+    if win_hands[bot_hand] == hands[user_hand-1]:
+        print("Bot win", win_hands[bot_hand])
+    elif bot_hand == hands[user_hand-1]:
+        print("Ничья")
+    else:
+        print("You win!")
+
+
+
+
 
